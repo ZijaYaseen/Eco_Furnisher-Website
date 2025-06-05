3c4c2bc8cd9e68b36574cc8ca698341132585f4b
Zija Yaseen <zijayaseen15@gmail.com>
2025-06-06 02:02:39 +0500

    update

--- my-app/src/components/Footer.tsx
+++ my-app/src/components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
       <div className="flex flex-wrap justify-between font-poppins pt-16 pb-8 px-7 md:px-16 lg:px-20 gap-10">
         {/* Address Section */}
         <div className="w-full lg:w-auto md:py-10 text-[#9F9F9F] font-normal text-base text-left">
-          400 University Drive Suite 200 Coral Gables, <br />
+          400 Company Furnisher Suite 200 Coral Gables, <br />
           FL 33134 USA
         </div>
 
@@ -68,7 +68,7 @@ const Footer = () => {
       {/* Bottom Border and Copyright */}
       <div className="w-[95%] sm:w-[85%] mx-auto border-t border-[#D9D9D9] flex justify-start">
         <p className="font-normal text-base text-start p-5">
-          2025 EcoFurnish Home Decor Website. All rights reserved.
+          EcoFurnish Home Decor Website. All rights reserved.
         </p>
       </div>
     </footer>
