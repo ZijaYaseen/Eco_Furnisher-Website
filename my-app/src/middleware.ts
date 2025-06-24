cd5a677dc9e4fa7fe4ef9efe14571258c75f728c
Zija Yaseen <zijayaseen15@gmail.com>
2025-06-24 15:33:11 +0500

    update'

--- my-app/src/middleware.ts
+++ my-app/src/middleware.ts
@@ -5,12 +5,16 @@ export const runtime = 'experimental-edge';
 
 export async function middleware(req: NextRequest) {
   try {
+    const isProd = process.env.NODE_ENV === "production";
+    const sessionCookieName = isProd 
+      ? "__Secure-next-auth.session-token" 
+      : "next-auth.session-token";
     const token = await getToken({ 
       req, 
       secret: process.env.NEXTAUTH_SECRET,
       // Explicitly specify cookie name
-      cookieName: "next-auth.session-token",
-      secureCookie: process.env.NODE_ENV === "production"
+      cookieName: sessionCookieName,
+      secureCookie: isProd
     });
 
     if (!token) {
